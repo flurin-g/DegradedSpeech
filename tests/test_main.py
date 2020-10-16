d05dbbab9abfe7add2f3cf33f5e28e7318ed9d46
Flurin Gishamer <fgishamer@me.com>
2020-10-16 11:23:15 +0200

    +test_copy_and_apply

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,26 @@
 from unittest import TestCase
-
+import soundfile as sf
 from main import create_copy_and_apply
+
+import os
+
 
 
 class Test(TestCase):
     def test_create_copy_and_apply(self):
-        copy_fn = create_copy_and_apply(lambda x: x)
-        copy_fn(src="tests/test_data/test-clean/61/70968/61-70968-0000.flac",
-                dst="tests/test_data/test-clean-copy/61/70968/61-70968-0000.flac")
+        copy_fn = create_copy_and_apply(lambda x: (x, 16_000))
+        print(type(copy_fn))
+        assert(copy_fn, callable)
+
+    def test_open_file(self):
+        print(os.getcwd())
+        file, sr = sf.read("test_data/test-clean/61/70968/61-70968-0000.flac")
+
+    def test_copy_and_apply(self):
+        def identity_audio(src):
+            data, samplerate = sf.read(src)
+            return data, samplerate
+        cwd = os.getcwd()
+        copy_fn = create_copy_and_apply(identity_audio)
+        copy_fn(src=cwd + "/test_data/test-clean/61/70968/61-70968-0000.flac",
+                dst=cwd + "/test_data/res.flac")
