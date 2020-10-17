75ad144d2a4b84de93d2cb42db60bf95963c899d
Flurin Gishamer <fgishamer@me.com>
2020-10-17 16:27:01 +0200

    +settings for reverberate

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
+import pathlib
 from shutil import copyfile, copytree
 
 import hydra
 from omegaconf import DictConfig, OmegaConf
 import numpy as np
 import soundfile as sf
+
+from add_noise import AddNoise
+from reverberate import create_ir_dataframe, Reverberate
+
+CWD = pathlib.Path(__file__).parent.absolute()
 
 
 def write_file(edited_file: np.ndarray, dst: str, sample_rate: int):
@@ -29,7 +35,27 @@ def create_copy_and_apply(func: callable):
 def main(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
 
-    copytree(cfg.source_directory, cfg.target_directory)
+    source_directory = f'{CWD}/{cfg.source_directory}'
+
+    if cfg.task == "add_noise":
+        fx = AddNoise(cfg, CWD)
+        target_dir = f'{CWD}/{cfg.target_directory}/noise'
+    elif cfg.task == "reverberate":
+        fx = Reverberate(cfg, CWD)
+        target_dir = f'{CWD}/{cfg.target_directory}/reverb'
+    elif cfg.task == "freq_filter":
+        fx = None
+        target_dir = f'{CWD}/{cfg.target_directory}/freq_filter'
+    elif cfg.task == "create_ir_meta":
+        df_ir = create_ir_dataframe(meta_path=cfg.ir_meta_path,
+                                    ir_path=cfg.impulse_response_path,
+                                    cwd=CWD)
+        print(df_ir.head())
+        return
+
+    apply_fx = create_copy_and_apply(fx)
+
+    copytree(source_directory, target_dir, copy_function=apply_fx, dirs_exist_ok=True)
 
 
 if __name__ == "__main__":
