5473bde9fd8fc90297a9ef28fe2c798ed047ec89
Flurin Gishamer <fgishamer@me.com>
2020-10-20 06:18:39 +0200

    extracted get_task()

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 import soundfile as sf
 
 from add_noise import AddNoise
+from dtln_de_noise import DtlnDeNoise
 from freq_filter import FreqFilter
 from reverberate import create_ir_dataframe, Reverberate
 
@@ -32,31 +33,41 @@ def create_copy_and_apply(func: callable):
     return copy_and_apply
 
 
+def get_task(cfg):
+    target_base = f'{CWD}/{cfg.target_directory}'
+    targets = {
+        "add_noise": (f'{target_base}/noise', AddNoise),
+        "reverberate": (f'{target_base}/reverb', Reverberate),
+        "freq_filter": (f'{target_base}/freq_filter', FreqFilter),
+    }
+
+    if cfg.task == "de_noise":
+        task = (f'{target_base}/de_noised-{cfg.de_noise}', DtlnDeNoise)
+        source_directory = targets[cfg.de_noise][0]
+    else:
+        task = targets[cfg.task]
+        source_directory = f'{CWD}/{cfg.source_directory}'
+    target_directory = task[0]
+    fx = task[1]
+
+    return fx, source_directory, target_directory
+
+
 @hydra.main(config_path="conf", config_name="config")
 def main(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
 
-    source_directory = f'{CWD}/{cfg.source_directory}'
-
-    if cfg.task == "add_noise":
-        fx = AddNoise(cfg, CWD)
-        target_dir = f'{CWD}/{cfg.target_directory}/noise'
-    elif cfg.task == "reverberate":
-        fx = Reverberate(cfg, CWD)
-        target_dir = f'{CWD}/{cfg.target_directory}/reverb'
-    elif cfg.task == "freq_filter":
-        fx = FreqFilter(cfg, CWD)
-        target_dir = f'{CWD}/{cfg.target_directory}/freq_filter'
-    elif cfg.task == "create_ir_meta":
+    if cfg.task == "create_ir_meta":
         df_ir = create_ir_dataframe(meta_path=cfg.ir_meta_path,
                                     ir_path=cfg.impulse_response_path,
                                     cwd=CWD)
         print(df_ir.head())
-        return
+    else:
+        fx, source_directory, target_directory = get_task(cfg)
 
-    apply_fx = create_copy_and_apply(fx)
+        apply_fx = create_copy_and_apply(fx(cfg, CWD))
 
-    copytree(source_directory, target_dir, copy_function=apply_fx, dirs_exist_ok=True)
+        copytree(source_directory, target_directory, copy_function=apply_fx, dirs_exist_ok=True)
 
 
 if __name__ == "__main__":
